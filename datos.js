82924b5b07ac147642bad483a40433e62ada83bd
julia rs <julia.kaova@gmail.com>
2025-05-19 14:37:23 +0200

    funciones de base de datos

---
+++ datos.js
@@ -0,0 +1,79 @@
+import dotenv from"dotenv";
+dotenv.config();
+
+import postgres from "postgres";
+
+
+function conectar(){
+    return postgres({
+        host : process.env.DB_HOST,
+        database : process.env.DB_NAME,
+        user : process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+    });
+}
+
+export function leerTareas(){
+    return new Promise((ok,ko)=> {
+        const conexion = conectar();
+
+        conexion `SELECT * FROM tareas ORDER BY id`
+        .then(tareas => {// aquí ya están las tareas, ya tengo esa info por eso corto conexión en la siguiente línea
+            conexion.end();   
+            ok(tareas)
+        })
+        .catch( error => ko({ error : "error en la base de datos"}))
+    });
+}
+
+export function crearTarea(tarea){
+    return new Promise((ok,ko)=> {
+        const conexion = conectar();
+
+        conexion `INSERT INTO tareas (tarea) VALUES (${tarea}) RETURNING id`
+        .then(([{id}])=> {
+            conexion.end();   
+            ok(id)
+        })
+        .catch( error =>  ko({ error : "error en la base de datos"}))
+    });
+}
+
+export function actualizarEstado(id){
+    return new Promise((ok,ko)=> {
+        const conexion = conectar();
+
+        conexion `UPDATE tareas SET terminada = NOT terminada WHERE id = ${id}`
+        .then( ({count})=> {        // revisar desestructuración
+            conexion.end();   
+            ok(count)
+        })
+        .catch( error =>  ko({ error : "error en la base de datos"}))
+    });
+}
+
+export function actualizarTexto(id,tarea){
+    return new Promise((ok,ko)=> {
+        const conexion = conectar();
+
+        conexion `UPDATE tareas SET tarea = ${tarea} WHERE id = ${id}`
+        .then( ({count})=> {   
+            conexion.end();   
+            ok(count)
+        })
+        .catch( error =>  ko({ error : "error en la base de datos"}))
+    });
+}
+
+export function actualizarTexto(id){
+    return new Promise((ok,ko)=> {
+        const conexion = conectar();
+
+        conexion `DELETE FROM tareas WHERE id = ${id}`
+        .then( ({count})=> {   
+            conexion.end();   
+            ok(count)
+        })
+        .catch( error =>  ko({ error : "error en la base de datos"}))
+    });
+ }
